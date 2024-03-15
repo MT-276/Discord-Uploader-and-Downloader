4ff1980d3f743a37e27acecab6077558b2d4e3c5
Meit Sant <meitsant@gmail.com>
2024-03-15 19:55:52 +0530

    Fixed Zipping and Unzipping issues

--- Functions.py
+++ Functions.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Name:        Main.py
+# Name:        Functions.py
 # Purpose:     Functions for Discord Uploader and Downloader
 #
 # Author:      Meit Sant
@@ -7,9 +7,11 @@
 # Created:     13 03 2024
 #
 # Lead Dev : Meit Sant
+# Testing  : Roshan Boby
 #-------------------------------------------------------------------------------
 import os, sys
-import json, zipfile, requests
+import json, requests
+from zipfile import ZipFile, ZIP_DEFLATED
 from discord_webhook import DiscordWebhook
 from tkinter.filedialog import askopenfilename
 
@@ -49,18 +51,48 @@ def upload_files(webhook_url,thread_id,folder_path):
     for file in files:
         file_dict = send_file(webhook_url,thread_id,folder_path,file,file_dict)
     
+    # Generating the Key string
     Str = json.dumps(file_dict)
+    file_dict = eval(Str)
+    New_dict = {}
+    for i in file_dict:
+        New_dict['File_Name'] = i[:-8]
+        break
+
+    for i in file_dict:
+        New_dict[i[-3:]] = file_dict[i][39:]
     
     send_message(webhook_url,thread_id,f"  **   **")
-    send_message(webhook_url,thread_id,f"Key for Downloading. Please Copy-Paste this key in the program to download the files.```{Str}```")
 
+    
+    str_dict = str(New_dict)
+    
+    if len(str_dict) > 2000:
+        # Write the key to a file
+        with open(f"Key_{New_dict['File_Name']}.txt", "w") as f:
+            f.write(str_dict)
+        send_message(webhook_url,thread_id,f"Key too large. Key was saved in uploader's pc.")
+        return
+    send_message(webhook_url,thread_id,f"Key for Downloading. Please Copy-Paste this key in the program to download the files.")
+    send_message(webhook_url,thread_id,f"```{str_dict}```")
+    
 def download_files(webhook_url,thread_id,string):
     folder_path = "./Downloads/RAW/"
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     
     file_dict = eval(string)
-
+    #print(file_dict)
+    
+    new_file_dict = {}
+    
+    for i in file_dict:
+        if i == 'File_Name':
+            continue
+        new_file_dict[f"{file_dict['File_Name']}.zip.{i}"] = f"https://cdn.discordapp.com/attachments/{file_dict[i]}"
+    
+    file_dict = new_file_dict
+    
     for num,i in enumerate(file_dict):
         url = file_dict[i]
         print(f"{num+1}. Downloading {i}...")
@@ -72,25 +104,47 @@ def download_files(webhook_url,thread_id,string):
         print(f"Downloaded.")
         
 def zip_and_split(file_path):
-    # Get the base file name
-    base_file_name = os.path.basename(file_path)
-    base_file_name = base_file_name
-
-    # Creating a new folder "Zipped" to store the zipped and split files
-    folder_path = "./Zipped/"
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
+    
+    # If the path is of a folder, then zip the folder
+    if os.path.isdir(file_path):
+        # Get the base folder name
+        folder_name = os.path.basename(file_path)
+        # Creating a new folder "Zipped" to store the zipped and split files
+        folder_path = "./Zipped/"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        # Zip the folder
+        with ZipFile(f'{folder_path}/{folder_name}.zip', 'w') as zip_archive:
+            for foldername, subfolders, filenames in os.walk(file_path):
+                for filename in filenames:
+                    # Create a complete filepath
+                    file_path = os.path.join(foldername, filename)
+                    # Add file to zip
+                    zip_archive.write(file_path, os.path.relpath(file_path, file_path))
+        file_path = f"{folder_path}/{folder_name}.zip"
+        path_of_zip_file = file_path
+    else:
+        # Creating a new folder "Zipped" to store the zipped and split files
+        folder_path = "./Zipped/"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            
+        filename = os.path.basename(file_path)
+        # Open the archive in write mode
+        with ZipFile(f'{folder_path}/{filename}.zip', 'w') as zip_archive:
+            # Add only the file (not the parent directory)
+            zip_archive.write(file_path, filename)  # Specify filename within the archive
+        path_of_zip_file = f"{folder_path}/{filename}.zip"
         
-    # Create a zip file
-    with zipfile.ZipFile(f"{folder_path}/{base_file_name}.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
-        zipf.write(file_path)
-
+    filename = os.path.basename(file_path)
+    base_file_name = filename
+    
     # Split the zip file into chunks
-    with open(f"{folder_path}/{base_file_name}.zip", "rb") as f_in:
+    with open(path_of_zip_file, "rb") as f_in:
         byte = f_in.read(1)
         chunk_count = 1
         while byte:
-            chunk_file_name = f"{folder_path}/{base_file_name}.zip.{str(chunk_count).zfill(3)}"
+            chunk_file_name = f"{folder_path}/{base_file_name}.{str(chunk_count).zfill(3)}"
             with open(chunk_file_name, "wb") as f_out:
                 f_out.write(byte)
                 Chunk = int(24.9 * 1024.0 * 1024.0) +1
@@ -101,7 +155,8 @@ def zip_and_split(file_path):
                     f_out.write(byte)
             chunk_count += 1
             byte = f_in.read(1)
-    os.remove(f"{folder_path}/{base_file_name}.zip")
+
+    os.remove(path_of_zip_file)
 
 def Choose_File(Type):
     '''
@@ -124,11 +179,36 @@ def zip_merge():
     folder_path = "./Downloads/RAW/"
     
     # Arrange files in ascended order
-    files = os.listdir(folder_path)
-    files.sort()
+    file_names_list = os.listdir(folder_path)
+    file_names_list.sort()
 
     # Unzip the files
-    with zipfile.ZipFile(f"./Downloads/{files[0][:-8]}", "w", zipfile.ZIP_DEFLATED) as zipf:
-        for file in files:
-            zipf.write(f"{folder_path}/{file}")
+    base_file_name = file_names_list[0][:-8]
 
+    # Concatenate
+    with open(f"{folder_path[:-4]}{base_file_name}.zip", "wb") as outfile:
+        for i in file_names_list:
+            with open(f"{folder_path}{i}", "rb") as infile:
+                outfile.write(infile.read())
+
+    print(f"\nFile {base_file_name}.zip merged successfully.")
+    print(f"Unzipping {base_file_name}.zip...")
+    
+    with ZipFile(f"{folder_path[:-4]}{base_file_name}.zip", "r") as zip_ref:
+        try:
+            zip_ref.extractall(folder_path[:-4])
+            os.remove(f"{folder_path[:-4]}{base_file_name}.zip")
+
+        except IsADirectoryError:
+            # Checking if a folder with the same name as the base file name already exists
+            if os.path.exists(f"./{base_file_name}"):
+                print(f"[ERROR] A folder with the name {base_file_name} already exists.")
+                zip_ref.extractall(f"./{base_file_name}_New")
+            else:
+                zip_ref.extractall(f"./{base_file_name}")
+            
+        
+        except:
+            print(f"[ERROR] Could not extract {base_file_name}.zip")
+            sys.exit()
+    
--- Main.py
+++ Main.py
@@ -13,12 +13,12 @@ import os
 import sys
 from Functions import *
 
-webhook_url = "https://discord.com/api/webhooks/1217386966065090590/ZHy6_elF8KG_n2jKWIPYOOhno3K16tvGEhoNlPCxSbRvB4dV6xlmgUwn0zVS27gI6qZl"
+webhook_url = "https://discord.com/api/webhooks/1217752661865267221/0l_VziYJg_M9NuPjryJ9nIKv9OJKmq5axN4Qu-gZxezpHeZg9laFCl65eaqmgMVd4eAT"
 
-thread_id = "1217543005926326382"
+thread_id = "1218202729340469368"
 
 
-print('Discord Uploader and Downloader V1.2')
+print('Discord Uploader and Downloader V1.4')
 print('Developed by     : Meit Sant')
 print('Licence          : MIT')
 
@@ -28,52 +28,107 @@ print('Licence          : MIT')
 option = input('\nUpload [U] or Download [D] : ')
 
 if option in ['U','u','Upload','upload']:
+
     option = input('\nUpload a file [A] or a folder [B] : ')
+
     if option in ['A','a','File','file']:
         file_path = input('\nEnter the file path : ')
+
         if file_path == "":
             print('\n[ERROR] Invalid file path. Exiting...')
             sys.exit()
+
+        file_path = file_path.replace('"','')
+
         #Check if file exists
         if not os.path.isfile(file_path):
-            print(f"\n[ERROR] File {file_path} does not exist.")
+            print(f"\n[ERROR] Could not find {file_path}. Exiting...")
             sys.exit()
         print('\nZipping and splitting file...')
         zip_and_split(file_path)
         print('Zipped.\n\nUploading files...\n')
         upload_files(webhook_url,thread_id,"./Zipped/")
-        
+
+
         # Delete the zipped folder
         for i in os.listdir("./Zipped/"):
             os.remove(f"./Zipped/{i}")
         os.rmdir("./Zipped/")
+
         print('\nFiles uploaded. Exiting...')
-        
-    
-    elif option in ['B','b','Folder','folder']:
-        folder_path = input('\nEnter the folder path : ')
-        print('\nUploading files...\n')
-        upload_files(webhook_url,thread_id,folder_path)
-        print('\nFiles uploaded. Exiting...')
-    else:
-        print('Invalid option. Exiting...')
         sys.exit()
 
-elif option in ['D','d','Download','download']:
-    string = input('\nEnter the key string : ')
-    
+    if option in ['B','b','Folder','folder']:
+        folder_path = input('\nEnter the folder path : ')
+        folder_path = folder_path.replace('"','')
+
+        #Check if folder exists
+        if not os.path.isdir(folder_path):
+            print(f"\n[ERROR] Could not find {folder_path}. Exiting...")
+            sys.exit()
+
+        option = input('\nDo you want to upload \n- The contents of the folder [A] or \n- Folder as a zip [B] : ')
+
+        if option in ['A','a','Contents','contents']:
+            print('\nUploading files...\n')
+            upload_files(webhook_url,thread_id,folder_path)
+            print('\nFiles uploaded. Exiting...')
+            sys.exit()
+
+        if option in ['B','b','Zip','zip']:
+            print('\nZipping and splitting folder...')
+            zip_and_split(folder_path)
+            print('Zipped.\n\nUploading files...\n')
+            upload_files(webhook_url,thread_id,"./Zipped/")
+
+            # Delete the zipped folder
+            for i in os.listdir("./Zipped/"):
+                os.remove(f"./Zipped/{i}")
+            os.rmdir("./Zipped/")
+
+            print('\nFiles uploaded. Exiting...')
+            sys.exit()
+
+    else:
+        print('[ERROR] Invalid option. Exiting...')
+        sys.exit()
+
+if option in ['D','d','Download','download']:
+    string = input('\nEnter the key or or directly the path of the file containing the Key \n->')
+
+    # Checking if the entered string is empty.
+    if string == "":
+        print('\n[ERROR] Invalid key. Exiting...')
+        sys.exit()
+
+    # Checking if the entered string is a path to the file containing the key.
+    if string[0] != "{":
+        string = string.replace('"','')
+        if os.path.isfile(string):
+            # Checking if the file is a txt.
+            if string[-4:] != ".txt":
+                print('\n[ERROR] Invalid file. It needs to be a .txt file. Exiting...')
+                sys.exit()
+
+            with open(string, "r") as f:
+                string = f.read()
+        else:
+            print('\n[ERROR] Could not find the file. Exiting...')
+            sys.exit()
+
     print('\nDownloading files...\n')
     download_files(webhook_url,thread_id,string)
 
-    
+
     try:
-        os.rmdir("./Downloads/RAW/")    
+        # Checks if the RAW folder is empty
+        os.rmdir("./Downloads/RAW/")
         print("\nFiles downloaded. Exiting...")
     except:
         print("\nFiles downloaded. Merging files...")
         zip_merge()
         print("\nFiles merged.\nOriginal reconstructed successfully. Exiting...")
-        
+
         # Delete the RAW folder
         for file in os.listdir("./Downloads/RAW/"):
             os.remove(f"./Downloads/RAW/{file}")
